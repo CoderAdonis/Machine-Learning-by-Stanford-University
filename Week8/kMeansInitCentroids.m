cc1bf6f27e069f43310f2341bf44dbdb7474e0e0
abhinavtembulkar <abhinavtembulkar@gmail.com>
2023-09-30 14:43:12 +0530

    All week 8 excercises

---
+++ Week8/kMeansInitCentroids.m
@@ -0,0 +1,25 @@
+function centroids = kMeansInitCentroids(X, K)
+%KMEANSINITCENTROIDS This function initializes K centroids that are to be 
+%used in K-Means on the dataset X
+%   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
+%   used with the K-Means on the dataset X
+%
+
+% You should return this values correctly
+centroids = zeros(K, size(X, 2));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should set centroids to randomly chosen examples from
+%               the dataset X
+%
+
+% We can use one array to index another
+
+m = size(X,2);
+rand_idxs = randi(m,K,1);
+centroids = X(rand_idxs(:),:);
+
+% =============================================================
+
+end
+
---
+++ Week8/main.m
@@ -0,0 +1,4 @@
+% source ex7.m
+% source ex7_pca.m
+
+source submit.m
